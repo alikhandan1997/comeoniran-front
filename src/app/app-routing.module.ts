4a32d2c40538b2ca85abc96145ae00a603b01219
aqakhandan <isateas2020@gmail.com>
2020-03-04 00:33:49 +0330

    adding home page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
+  { path: '' , component: HomePageComponent },
   { path: 'login', component: LoginComponent }
 ];
 
