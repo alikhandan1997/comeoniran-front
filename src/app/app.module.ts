adcfa92d9530727232c650bcd531b2116f83270b
aqakhandan <isateas2020@gmail.com>
2020-03-04 00:35:46 +0330

    adding new components footer navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavigationComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
