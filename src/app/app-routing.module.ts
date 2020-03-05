a4158fad93c59fc17a71a4e4edbb5c1b5c937d2e
aqakhandan <isateas2020@gmail.com>
2020-03-06 00:26:15 +0330

    adding new modules to routing (destinations,trip types)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { TripTypesComponent } from './trip-types/trip-types.component';
 
 const routes: Routes = [
   { path: '' , component: HomePageComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'destinations', component: DestinationsComponent },
+  { path: 'triptypes', component: TripTypesComponent }
 ];
 
 @NgModule({
