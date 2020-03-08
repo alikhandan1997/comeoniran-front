87211cd72fb970151776ad3578e2397caaf91370
aqakhandan <isateas2020@gmail.com>
2020-03-08 18:23:17 +0330

    adding new modules to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { DestinationsComponent } from './destinations/destinations.component';
 import { TripTypesComponent } from './trip-types/trip-types.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CitiesComponent } from './cities/cities.component';
 
 const routes: Routes = [
   { path: '' , component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'destinations', component: DestinationsComponent },
-  { path: 'triptypes', component: TripTypesComponent }
+  { path: 'where-to-go', component: TripTypesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'cities', component: CitiesComponent }
+
 ];
 
 @NgModule({
