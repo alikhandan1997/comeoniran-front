36ce463924054e6ace6fa4af37f08c52b41574dd
aqakhandan <isateas2020@gmail.com>
2020-03-13 14:57:08 +0330

    adding disable footer when in login page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,7 +9,19 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
+  href: string;
+  hrefPart: string[];
+
+  showFooter: boolean = true;
+
   ngOnInit() {
+    this.href = window.location.href;
+    this.hrefPart = this.href.split('/');
+    console.log(this.href);
+    
+    if(this.hrefPart[3] == 'login' || this.hrefPart[3] == 'register' || this.hrefPart[3] == 'resetpassword') {
+      this.showFooter = !this.showFooter;
+    }
   }
 
 }
