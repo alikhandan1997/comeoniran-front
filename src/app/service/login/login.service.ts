c23dfe65a0e2cde69ee544e506af58cda8871586
aqakhandan <aqakhandan@aqakhandan.khandan>
2020-03-16 20:45:00 +0330

    a

--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import ApiMap from '../../apiMap/apiMap.json';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,19 @@ import { Observable } from 'rxjs';
 export class LoginService {
 
   private baseurl = 'http://192.168.43.116:8585/';
+  apiUrl: string;
 
   constructor(private http: HttpClient) { }
 
   checkUser(data: string): Observable<Object> {
-    return this.http.post(`${this.baseurl} + 'api/login/'`, data);
+    this.apiUrl = ApiMap.api.loginApi.checkUser
+    return this.http.post(`${this.baseurl}${this.apiUrl}`, data);
+    
   }
 
   registerUser(data: string): Observable<Object> {
-    return this.http.post(`${this.baseurl} + 'api/register/'`, data);
+    this.apiUrl = ApiMap.api.loginApi.registerUser;
+    return this.http.post(`${this.baseurl}${this.apiUrl}`, data);
   }
 
 }
