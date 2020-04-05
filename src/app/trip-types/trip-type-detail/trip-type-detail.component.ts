af2d4de04fbd2a83b8da9f04462249528ef8d1e2
aqakhandan <Amirmohammad.shakeri@yahoo.com>
2020-04-05 20:47:15 +0430

    adding trip type service function and check with url to get the trip type detail

--- src/app/trip-types/trip-type-detail/trip-type-detail.component.ts
+++ src/app/trip-types/trip-type-detail/trip-type-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TripTypesService } from 'src/app/service/trip-types/trip-types.service';
 
 @Component({
   selector: 'app-trip-type-detail',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripTypeDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private typeService: TripTypesService) { }
+
+  href: string;
+  hrefPart: string[];
+  pageCity: string;
+
+  filter: string;
+
+  types = [];
+
+  typeName: string;
+  typeImage: string;
 
   ngOnInit() {
+    this.href = window.location.href;
+    this.hrefPart = this.href.split('/');
+    this.pageCity = this.hrefPart[4];
+    console.log(this.pageCity)
+    this.getTripTypes();
+  }
+
+  getTripTypes = () => {
+    this.filter = '';
+    this.typeService.getTripTypes(this.filter).subscribe((data) => {
+      this.types = Array.from(Object.keys(data['result']), k => data['result'][k]);
+      console.log('toptype', this.types);
+      for(let i=0; i<this.types.length; i++) {
+        if(this.types[i].name == this.pageCity) {
+          this.typeName = this.types[i].name;
+          this.typeImage = this.types[i].image;
+        }
+      }
+    })
   }
 
 }
