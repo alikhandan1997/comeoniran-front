0ddb4ab1b0e666922b6c350d9d62a59f427c7c6e
aqakhandan <Amirmohammad.shakeri@yahoo.com>
2020-04-05 20:32:46 +0430

    adding get type function and get top 4 trip type to display at top

--- src/app/trip-types/trip-types/trip-types.component.ts
+++ src/app/trip-types/trip-types/trip-types.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TripTypesService } from 'src/app/service/trip-types/trip-types.service';
 
 @Component({
   selector: 'app-trip-types',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripTypesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private typeService: TripTypesService) { }
+
+  filter: string;
+
+  topType = [];
+
+  otherType = [];
 
   ngOnInit() {
+    this.getTripTypes();
+  }
+
+  getTripTypes = () => {
+    this.filter = '?top=4';
+    this.typeService.getTripTypes(this.filter).subscribe((data) => {
+      this.topType = Array.from(Object.keys(data['result']), k => data['result'][k]);
+      console.log('toptype', this.topType);
+    })
   }
 
 }
