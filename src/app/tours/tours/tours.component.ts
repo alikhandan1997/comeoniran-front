13901158eadeeff925dee0f3f4a2489e3c3fd425
aqakhandan <Amirmohammad.shakeri@yahoo.com>
2020-04-06 18:01:39 +0430

    adding get tours leader api function and when click scroll top

--- src/app/tours/tours/tours.component.ts
+++ src/app/tours/tours/tours.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from 'src/app/service/serviceis/services.service';
 
 @Component({
   selector: 'app-tours',
@@ -7,21 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToursComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tourService: ServicesService) { }
 
   showOthers: boolean = false;
-  lessMore:string = 'More';
+  lessMore: string = 'More';
+
+  filter: string;
+
+  topTours = [];
+  otherTours = [];
 
   ngOnInit() {
+    this.getTours();
   }
 
   showOther = () => {
     if(this.showOthers == false) {
       this.lessMore = 'Less';
     } else {
-      this.lessMore = 'More'
+      this.lessMore = 'More';
+      window.scroll(0,0);
     }
     this.showOthers = !this.showOthers;
+  }
+
+  getTours = () => {
+    this.filter = '?type=tour&top=4';
+    this.tourService.getService(this.filter).subscribe((data) => {
+      this.topTours = Array.from(Object.keys(data['result']), k => data['result'][k]);
+      console.log('top', this.topTours);
+    })
+    this.filter = '?type=tour';
+    this.tourService.getService(this.filter).subscribe((data) => {
+      this.otherTours = Array.from(Object.keys(data['result']), k => data['result'][k]);
+      console.log('last', this.otherTours);
+    })
   }
 
 }
